489020f397f59413c64afcdc7ca6f5f191ba4496
Bluenarysun <tee.emilond@gmail.com>
2021-03-09 12:48:34 +0700

    Test cy.visit

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,14 +1,5 @@
-describe('My first simple test with cypress', () => {
-  it('True should be True', () => {
-    expect(true).to.equal(true);
-  });
-  it('5 should be 5', () => {
-    expect(5).to.equal(5);
-  });
-});
-
-describe('Another describe block', () => {
-  it('False should be false', () => {
-    expect(false).to.equal(false);
+describe('Browser Actions', () => {
+  it('Should load correct url', () => {
+    cy.visit('https://example.com', { timeout: 10000 });
   });
 });
