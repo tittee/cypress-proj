00c4588db401661ff6b8b192ebcb7a7db4e771e0
Bluenarysun <tee.emilond@gmail.com>
2021-03-10 14:11:16 +0700

    Working with json

---
+++ cypress/integration/examples/fixture.spec.js
@@ -0,0 +1,11 @@
+describe('Login with Fixtures Data', () => {
+  it('should try to login', () => {
+    cy.visit('http://zero.webappsecurity.com/login.html');
+    cy.fixture('user').then(user => {
+      const username = user.username;
+      const password = user.password;
+
+      cy.get('#user_login').type(username);
+    });
+  });
+});
--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,5 +1,10 @@
 describe('Browser Actions', () => {
-  it('Should load correct url', () => {
-    cy.visit('https://example.com', { timeout: 10000 });
+  it('Should be load books website', () => {
+    cy.visit('https://bluenary.com/', { timeout: 10000 });
+    cy.url().should('include', '/');
+  });
+
+  it('should click menu เกี่ยวกับเรา', () => {
+    cy.get('a.navbar-item').contains('เกี่ยวกับเรา').click();
   });
 });
