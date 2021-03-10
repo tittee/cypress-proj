924cce73eeb947f4851b2da2dc9be30584297b00
Bluenarysun <tee.emilond@gmail.com>
2021-03-10 17:45:16 +0700

    Read files

--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -9,4 +9,12 @@ describe('Write / Read Data to JSON / Text file', () => {
   it('should write data into .txt file', () => {
     cy.writeFile('log.txt', 'My name is Wittawat Kittiwarabud');
   });
+
+  it('should read and verify data from JSON file', () => {
+    cy.readFile('log.json').its('age').should('eq', 25);
+  });
+
+  it('should read and verify data from the text file', () => {
+    cy.readFile('log.txt').should('eq', 'My name is Wittawat Kittiwarabud');
+  });
 });
